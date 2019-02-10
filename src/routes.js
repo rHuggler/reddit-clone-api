4bf87feaa98469493a9b22b4b02fb36ed237ffa1
Ronald Huggler <ronald@huggler.com.br>
2019-02-10 20:43:33 -0200

    Add post route for Post

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ const express = require('express')
 const routes = express.Router()
 
 const { userController } = require('./controllers/userController')
+const { postController } = require('./controllers/postController')
 
 routes.post('/user', userController.post)
+routes.post('/post', postController.post)
 
 module.exports = {
     routes
