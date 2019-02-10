ec08d4ea4f3500a35f1e9485743b8dca476dae7b
Ronald Huggler <ronald@huggler.com.br>
2019-02-10 20:01:34 -0200

    Add body-parser

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const { routes } = require('./routes')
 
 const app = express()
-const db = mongoose.connect('mongodb://mongodb:27017/redditCloneApi')
+const db = mongoose.connect('mongodb://localhost:27017/redditCloneApi', {useNewUrlParser: true})
 
-app.use('/', routes)
+app.use(bodyParser.json())
+app.use('/api', routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}/`)
