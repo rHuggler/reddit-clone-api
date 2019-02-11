e762831434845e3dd661c85b7b7c36d82f284dda
Ronald Huggler <ronald@huggler.com.br>
2019-02-10 22:22:27 -0200

    Create post route for Comment

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ const routes = express.Router()
 
 const { userController } = require('./controllers/userController')
 const { postController } = require('./controllers/postController')
+const { commentController } = require('./controllers/commentController')
 
 routes.post('/user', userController.post)
 routes.post('/post', postController.post)
+routes.post('/comment', commentController.post)
 routes.get('/post', postController.getAll)
 
 module.exports = {
